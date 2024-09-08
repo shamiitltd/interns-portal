87eaf5f6d624508f32b2d655cea914003c8e9664
desaiHarshit722 <114604298+desaiHarshit722@users.noreply.github.com>
2024-09-08 19:13:18 +0530

    fourth-commit

---
+++ src/components/dashboard/areaTable/AreaTable.jsx
@@ -0,0 +1,116 @@
+import AreaTableAction from "./AreaTableAction";
+import "./AreaTable.scss";
+
+const TABLE_HEADS = [
+  "Name of candidate",
+  "Unique ID",
+  "Date",
+  "Morning Standup",
+  "Status",
+  "Evening Standup",
+  "Action",
+];
+
+const TABLE_DATA = [
+  {
+    id: 100,
+    name: "Harshit Desai",
+    order_id: "#11232",
+    date: "sept 6,2024",
+    customer: "Present",
+    status: "Joined",
+    amount: 400,
+  },
+  {
+    id: 101,
+    name: "Shubham Khorge",
+    order_id: "#16232",
+    date: "sept 6,2024",
+    customer: "Present",
+    status: "Joined",
+    amount: 288,
+  },
+  {
+    id: 102,
+    name: "Rohini Zire",
+    order_id: "#74454",
+    date: "sept 6,2024",
+    customer: "Absent",
+    status: "Not joined ",
+    amount: 500,
+  },
+  {
+    id: 103,
+    name: "Pranchal Sharma",
+    order_id: "#45754",
+    date: "sept 6,2024",
+    customer: "Present",
+    status: "Joined",
+    amount: 100,
+  },
+  {
+    id: 104,
+    name: "Shivayu Rane",
+    order_id: "11232",
+    date: "sept 6,2024",
+    customer: "Absent",
+    status: "Not Joined",
+    amount: 60,
+  },
+  {
+    id: 105,
+    name: "Mayur Ghangare",
+    order_id: "#656555",
+    date: "sept 6,2024",
+    customer: "Absent",
+    status: "Not Joined",
+    amount: 80,
+  },
+];
+
+const AreaTable = () => {
+  return (
+    <section className="content-area-table">
+      <div className="data-table-info">
+        <h4 className="data-table-title">Daily Attendance Tracker</h4>
+      </div>
+      <div className="data-table-diagram">
+        <table>
+          <thead>
+            <tr>
+              {TABLE_HEADS?.map((th, index) => (
+                <th key={index}>{th}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {TABLE_DATA?.map((dataItem) => {
+              return (
+                <tr key={dataItem.id}>
+                  <td>{dataItem.name}</td>
+                  <td>{dataItem.order_id}</td>
+                  <td>{dataItem.date}</td>
+                  <td>{dataItem.customer}</td>
+                  <td>
+                    <div className="dt-status">
+                      <span
+                        className={`dt-status-dot dot-${dataItem.status}`}
+                      ></span>
+                      <span className="dt-status-text">{dataItem.status}</span>
+                    </div>
+                  </td>
+                  <td>${dataItem.amount.toFixed(2)}</td>
+                  <td className="dt-cell-action">
+                    <AreaTableAction />
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
+
+export default AreaTable;
