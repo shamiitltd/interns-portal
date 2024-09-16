6c443a29f071f067d734bb0e35ebf30cba4b4771
Pranchal999 <pranchalsharma4321@gmail.com>
2024-09-16 19:32:57 +0530

    modified

--- Backend/certificates/certificats/certificate.py
+++ Backend/certificates/certificats/certificate.py
@@ -15,10 +15,10 @@ def generate_certificate():
     if not name or not course:
         return {'error': 'Name and course are required'}, 400
     
-   
+   # Create a byte stream buffer for the PDF
     buffer = BytesIO()
     
-    
+     # Create a PDF with ReportLab
     c = canvas.Canvas(buffer, pagesize=letter)
     
     
@@ -28,13 +28,13 @@ def generate_certificate():
     c.drawString(100, 600, f'has completed the Internship')
     c.drawString(100, 550, f'{course}')
     
-   
+     # Save the PDF
     c.save()
     
-
+    # Move to the beginning of the BytesIO buffer
     buffer.seek(0)
     
-    
+    # Return the PDF as a response
     return send_file(buffer, as_attachment=True, download_name='certificate.pdf', mimetype='application/pdf')
 
 if __name__ == '__main__':
